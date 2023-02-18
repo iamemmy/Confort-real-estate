d3c28e0b3427f5dbf25149049aa3b9aa3f054430
Samson Eze <38159339+iamemmy@users.noreply.github.com>
2023-02-18 12:44:32 +0100

    Worked on search bar
    Worked on search bar

--- Confort-RE/main.js
+++ Confort-RE/main.js
@@ -25,3 +25,36 @@ $(document).ready(function() {
       });
   });
   
+//COUNTER JS
+let ourCustomer = document.querySelector('#customers');
+let ourUnits = document.querySelector('#units');
+let ourUnitsTwo = document.querySelector('#units2');
+
+let count = 0;
+
+// window.addEventListener("onload", maincounter())
+
+function maincounter() {
+   let counterOne = setInterval(() => {
+        if (count == 100) {
+            clearInterval(counterOne)
+        }else{
+            ourCustomer.textContent = `${count++}k`;
+        }
+    }, 100);
+
+    let counterTwo =setInterval(() => {
+        if (count == 30) {
+            clearInterval(counterTwo)
+        }else{
+            ourUnits.textContent = `${count++}k`;
+        }
+    }, 100);
+    let counterThree =setInterval(() => {
+        if (count == 20) {
+            clearInterval(counterThree)
+        }else{
+            ourUnitsTwo.textContent = `${count++}k`;
+        }
+    }, 100);
+}
